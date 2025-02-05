09d293d131e109ae156fc6129be600bf8eb37962
AlexSyvo <killmore@poczta.fm>
2025-02-05 19:25:45 +0100

    lvl up, dmg atk version 0.1

--- src/GameCharacter.java
+++ src/GameCharacter.java
@@ -8,4 +8,13 @@ public abstract class GameCharacter {
         this.level = level;
         this.health = health;
     }
+    public int lvlUp(int level){
+        return level+=1;
+    }
+    public int dmg(int dmg){
+        return dmg;
+    }
+    public int atk(int dmg,String name){
+        return dmg;
+    }
 }
